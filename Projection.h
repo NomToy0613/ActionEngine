589d3af3cf7e35deba1236bdbb831dfa5e8320d1
PC_User <PC_User@>
2026-08-03 13:12:06 +0900

    プロジェクトファイルを追加。

---
+++ Projection.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "Vector3.h"
+#include "Quaternion.h"
+#include "Matrix4x4.h"
+
+class Projection {
+public:
+	float fov;    //画角
+	float aspect; //アスペクト比
+	float nearZ;  //前方クリップ面
+	float farZ;   //後方クリップ面
+
+	//コンストラクタ
+	Projection() :
+		fov(0.785398f),   //画角45度
+		aspect(16.0f / 9.0f),
+		nearZ(0.1f),
+		farZ(100.0f)
+	{}
+
+	Matrix4x4 projection() const;
+};
